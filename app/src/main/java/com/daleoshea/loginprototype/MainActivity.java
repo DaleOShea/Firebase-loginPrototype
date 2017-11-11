60e95473afbad27672bdbb252dcf21ce01c963a2
Dale O Shea <daleoshea@gmail.com>
2017-11-11 16:20:43 +0000

    Method to handle already registered users

--- app/src/main/java/com/daleoshea/loginprototype/MainActivity.java
+++ app/src/main/java/com/daleoshea/loginprototype/MainActivity.java
@@ -1,19 +1,105 @@
 package com.daleoshea.loginprototype;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     EditText emailEditText;
     EditText passwordEditText;
-    Button   signInButton;
+    Button   registerButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initalsiing the FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+       emailEditText = (EditText)findViewById(R.id.emailEditText);
+       passwordEditText = (EditText)findViewById(R.id.passwordEditText);
     }
+
+
+    //Checking to see if the user is currently signed in
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void createAccount(View view){
+
+        final String userEmail = emailEditText.getText().toString();
+        final String userPassword = passwordEditText.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.i("Tag", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Toast.makeText(MainActivity.this, "Welcome to myDives", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.i("TAG", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                });
+    }
+
+
+    public void signIn(View view){
+
+        final String userEmail = emailEditText.getText().toString();
+        final String userPassword = passwordEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.i("TAG", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Succesful", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.i("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+
+
 }
