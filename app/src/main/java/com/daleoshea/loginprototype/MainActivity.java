a2ee16618b0afd1aab8d2477afbe6b4a1df4b886
Dale O Shea <daleoshea@gmail.com>
2017-11-11 15:15:30 +0000

    Firebase added to project

--- app/src/main/java/com/daleoshea/loginprototype/MainActivity.java
+++ app/src/main/java/com/daleoshea/loginprototype/MainActivity.java
@@ -2,10 +2,14 @@ package com.daleoshea.loginprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    EditText emailEditText;
+    EditText passwordEditText;
+    Button   signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
